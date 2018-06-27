ec62940c7b5ee78a69a48f3f8063f47f6742f5ec
wowa42 <wolfgang@walznet.de>
2018-06-27 13:08:30 +0200

    P corrections

--- run.py
+++ run.py
@@ -2,13 +2,13 @@ from app import create_app, db
 from app.auth.models import User
 from sqlalchemy import exc
 
-flask_app = create_app('prod')
+flask_app = create_app('dev')
 with flask_app.app_context():
     db.create_all()
-    try:
-        if not User.query.filter_by(user_name='harry').first():
-            User.create_user(user='harry', email='harry@potters.com', password='secret')
-    except exc.IntegrityError:
-            flask_app.run()
+    #try:
+    if not User.query.filter_by(user_name='harry').first():
+        User.create_user(user='harry', email='harry@potters.com', password='secret')
+    #except exc.IntegrityError:
+    flask_app.run()
 
 
