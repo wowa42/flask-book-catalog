a8331d2557c867cce1a4a37e4be3453378cdb939
wowa42 <wolfgang@walznet.de>
2018-06-27 14:16:48 +0200

    run.py changed

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ from sqlalchemy import exc
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    #try:
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry', email='harry@potters.com', password='secret')
-    #except exc.IntegrityError:
-    flask_app.run()
+    try:
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry', email='harry@potters.com', password='secret')
+    except exc.IntegrityError:
+        flask_app.run()
 
 
