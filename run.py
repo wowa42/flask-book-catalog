c277a88957e9738efc4005c2f151dd9830608f24
wowa42 <wolfgang@walznet.de>
2018-06-27 13:57:56 +0200

    new pipp freeze

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app, db
 from app.auth.models import User
 from sqlalchemy import exc
 
-flask_app = create_app('dev')
+flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
     #try:
